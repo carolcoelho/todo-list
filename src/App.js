072210cd575247eefc54cffb74e433e977d56e6e
carolcoelho <cafernanda05@gmail.com>
2021-06-07 11:36:19 -0300

    Finalização de projeto

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+import TodoList from './componentes/TodoList';
+
+function App() {
+  return (
+    <div className="todo-app">
+      <TodoList />
+    </div>
+
+
+  )
+}
+
+export default App;
